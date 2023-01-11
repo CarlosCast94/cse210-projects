6c71b22561297d4929d6944c709725f7859bdd7e
Carlos Castañeda <carlos.alex94@hotmail.com>
2023-01-10 21:18:29 -0600

    new program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string grade = Console.ReadLine();
+        int RealGrade = int.Parse(grade);
+
+        string letter = "";
+
+        if (RealGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (RealGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (RealGrade>= 70)
+        {
+            letter = "C";
+        }
+        else if (RealGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
