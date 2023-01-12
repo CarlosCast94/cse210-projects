69a9c99f7a663e854d072b9b3fd9326f5776be91
Carlos Castañeda <carlos.alex94@hotmail.com>
2023-01-11 20:41:56 -0600

    Add code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {;
+        
+        Random Generator = new Random();
+        int Number = Generator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != Number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (Number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
