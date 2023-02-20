dc865efebe6868da53d253c161ced57880158f34
Carlos Castañeda <carlos.alex94@hotmail.com>
2023-02-20 14:50:23 -0600

    Desing of main menu and clasess

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,14 @@
+public class Activity
+{
+    private string _Activity;
+    private string _Description;
+    private int _Duration;
+
+    public Activity (string Activity, string Description, int Duration)
+    {
+        _Activity = Activity;
+        _Description = Description;
+        _Duration = Duration;
+    }
+    
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int opcion = 0;
+        while (opcion !=4)
+        {
+                Console.WriteLine("Select a choice from the menu:\n"+
+                "\n1.Start breathing activity"+
+                "\n2.Start reflecting activity"+
+                "\n3.Star listing activity"+
+                "\n4.Quit");
+                opcion = int.Parse(Console.ReadLine());
+                switch(opcion){
+
+
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                Console.WriteLine("Goodbye");
+                                break;
+                            default:
+                                Console.Write("Please enter a valid choice:\n");
+                                break;
+
+                    }
+        }
+
+
     }
 }
