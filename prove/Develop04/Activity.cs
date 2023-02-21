f452ba2f3ff7b1ebc71f09e0c381b0de513d4848
Carlos Castañeda <carlos.alex94@hotmail.com>
2023-02-20 20:08:05 -0600

    Add methods and other clasess

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -4,11 +4,18 @@ public class Activity
     private string _Description;
     private int _Duration;
 
-    public Activity (string Activity, string Description, int Duration)
+    public Activity (string Activity, string Description,int Duration)
     {
         _Activity = Activity;
         _Description = Description;
         _Duration = Duration;
     }
-    
+    public string DisplayStart()
+    {
+        return $"Welcome to the {_Activity} \n {_Description}";
+    }
+    public string DisplayEnding()
+    {
+        return $"Well done!! You have completed another {_Duration} seconds of the {_Activity}";
+    }
 }
